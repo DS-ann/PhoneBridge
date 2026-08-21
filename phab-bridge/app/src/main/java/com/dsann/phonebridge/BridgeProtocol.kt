9069d41008caf08c6a400d61597075340128acbf
DS-ann <dsannigrahi5956@gmail.com>
2026-08-21 21:54:54 +0530

    Add PhoneBridge command protocol

---
+++ phab-bridge/app/src/main/java/com/dsann/phonebridge/BridgeProtocol.kt
@@ -0,0 +1,11 @@
+package com.dsann.phonebridge
+
+object BridgeProtocol {
+    const val PORT = 45821
+
+    const val DIAL = "DIAL"
+    const val ANSWER = "ANSWER"
+    const val REJECT = "REJECT"
+    const val HANGUP = "HANGUP"
+    const val PING = "PING"
+}
