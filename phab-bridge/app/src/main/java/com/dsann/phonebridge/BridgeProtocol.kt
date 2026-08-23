1c62125cbd5528118dbe8ece77efa4b396c39f3b
DS-ann <dsannigrahi5956@gmail.com>
2026-08-23 12:33:00 +0530

    Reduce bridge protocol to Wi-Fi audio only

--- phab-bridge/app/src/main/java/com/dsann/phonebridge/BridgeProtocol.kt
+++ phab-bridge/app/src/main/java/com/dsann/phonebridge/BridgeProtocol.kt
@@ -2,10 +2,5 @@ package com.dsann.phonebridge
 
 object BridgeProtocol {
     const val PORT = 45821
-
-    const val DIAL = "DIAL"
-    const val ANSWER = "ANSWER"
-    const val REJECT = "REJECT"
-    const val HANGUP = "HANGUP"
     const val PING = "PING"
 }
